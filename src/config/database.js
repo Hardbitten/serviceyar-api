70c58d5c6530b7409a2b14f941270232b9c00f16
Mohammad Shabani <mohammad.shabani@partsoftware.com>
2021-06-19 13:52:19 +0430

    .

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,17 @@
-import mongoose from  "mongoose" ;
-
-const Connection = async ()=>{
-try {
-    const conn = await mongoose.connect(process.env.DATABASE_URI,{
-        useNewUrlParser:true,
-        useFindAndModify:true,
-        useUnifiedTopology:true
-    })
-    console.log(`database connected:${conn.connection.host}`);
-} catch (err) {
-    console.log(err);
-    process.exit(1)
-}
+import mongoose from "mongoose";
+const Connection = async () => {
+    try {
+        const DB = process.env.DATABASE_URI.replace('<password>',process.env.DATABASE_PASSWORD)
+        const conn = await mongoose.connect(DB, {
+            useNewUrlParser: true,
+            useFindAndModify: true,
+            useUnifiedTopology: true
+        })
+        console.log(`database connected: ${conn.connection.name}`);
+    } catch (err) {
+        console.log(err);
+        process.exit(1)
+    }
 
 }
 
