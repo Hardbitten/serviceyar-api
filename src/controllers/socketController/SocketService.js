4d1f81ee25067bbb8b7c1cb2c2cd3e5cc05e5675
Mohammad Shabani <mohammad.shabani@partsoftware.com>
2021-06-21 17:44:58 +0430

    .

--- src/controllers/socketController/SocketService.js
+++ src/controllers/socketController/SocketService.js
@@ -3,33 +3,41 @@ import User from "../../model/userModel";
 const userSockets = new Map();
 
 const SocketService = (socket) => {
-  if (socket.type === "user") {
-    //user
-    console.log("user connected");
+  try {
+    if (socket.type === "user") {
+      //user
+      console.log("user connected");
 
-    userSockets.set(socket.user.id, socket);
-    socket.on("disconnected", (socket) => {
-      console.log("user disconnected");
-    });
+      userSockets.set(socket.user.id, socket);
+      socket.on("disconnected", (socket) => {
+        console.log("user disconnected");
+      });
 
-    //########
-  } else if (socket.type === "driver") {
-    //driver
-    console.log("driver connected");
+      //########
+    } else if (socket.type === "driver") {
+      //driver
+      console.log("driver connected");
 
-    socket.on("onDriverLocationChange", async (data) => {
-      const users = await User.find({ driverId: socket.driver.id });
-      let usersKey = [...userSockets.entries()].map(([k]) => k);
-      for (const user of usersKey) {
-        const userSocket = userSockets.get(user);
-        if (userSocket) {
-          userSocket.emit("onDriverLocationChange", data);
+      socket.on("onDriverLocationChange", async (data) => {
+        const users = await User.find({
+          driverId: socket.driver.id
+        });
+        let usersKey = [...userSockets.entries()].map(([k]) => k);
+
+        for (const user of usersKey) {
+          const userSocket = userSockets.get(user);
+          if (userSocket) {
+            userSocket.emit("onDriverLocationChange", data);
+          }
         }
-      }
-    });
-    socket.on("disconnected", (socket) => {
-      console.log("driver disconnected");
-    });
+      });
+      socket.on("disconnected", (socket) => {
+        console.log("driver disconnected");
+      });
+    }
+  } catch (err) {
+    console.log("THIS IS FROM SOCKETSERVICE HANDDLER CATCH ERR", err);
+    return ;
   }
 };
 
